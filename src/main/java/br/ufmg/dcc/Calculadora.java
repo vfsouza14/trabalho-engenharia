4e0a3482966ab492b0a1eec5e0d566a501f9a09d
Victor <progfernandes@hotmail.com>
2021-06-15 19:47:36 -0300

    trabalho demo-ci

---
+++ src/main/java/br/ufmg/dcc/Calculadora.java
@@ -0,0 +1,13 @@
+package br.ufmg.dcc;
+
+public class Calculadora {
+
+  public int soma(int x, int y) {
+    return x + y + 5;//teste
+  }
+
+  public int subtrai(int x, int y) {
+    return x - y;
+  }
+
+}
